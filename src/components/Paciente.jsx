9b95f8cae57b33c808526322266680fefa0d09a6
Linda Paz <lindacsndelapaz@gmail.com>
2023-01-25 14:28:15 -0300

    add "show patient information"

--- src/components/Paciente.jsx
+++ src/components/Paciente.jsx
@@ -1,20 +1,23 @@
-const Paciente = () => {
+const Paciente = ({paciente}) => {
+
+    const {nombre, propietario, email, fecha, sintomas} = paciente // destructuring
+
     return (
         <div className="bg-white mx-3 my-5 shadow-md px-5 py-5 rounded-xl">
                 <p className="text-sm font-bold mb-5 text-gray-700 uppercase">Nombre: {''}
-                <span className="normal-case font-normal">Hook</span>
+                <span className="normal-case font-normal">{nombre}</span>
                 </p>
                 <p className="text-sm font-bold mb-5 text-gray-700 uppercase">Propietario: {''}
-                <span className="normal-case font-normal">Linda</span>
+                <span className="normal-case font-normal">{propietario}</span>
                 </p>
                 <p className="text-sm font-bold mb-5 text-gray-700 uppercase">Email: {''}
-                <span className="normal-case font-normal">lindacsndelapaz@gmail.com</span>
+                <span className="normal-case font-normal">{email}</span>
                 </p>
                 <p className="text-sm font-bold mb-5 text-gray-700 uppercase">Fecha alta: {''}
-                <span className="normal-case font-normal">10 de Diciembre 2022</span>
+                <span className="normal-case font-normal">{fecha}</span>
                 </p>
                 <p className="text-sm font-bold mb-5 text-gray-700 uppercase">Sintomas: {''}
-                <span className="normal-case font-normal">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cumque dolor odio ad, adipisci nulla modi eaque atque eos! A hic sunt vitae nesciunt non ipsam voluptates, aut ex officia debitis?</span>
+                <span className="normal-case font-normal">{sintomas}</span>
                 </p>
             </div>
         )
