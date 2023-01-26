0d3bb4307dcece762ce53ef8acb9de7c6589a635
Linda Paz <lindacsndelapaz@gmail.com>
2023-01-26 12:04:35 -0300

    add localStorage for patient

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import Header from "./components/Header"
 import Formulario from "./components/Formulario"
 import ListadoPacientes from "./components/ListadoPacientes"
@@ -8,6 +8,10 @@ function App() {
 
   const [pacientes, setPacientes] = useState([]); // arreglo de paciente
   const [paciente, setPaciente] = useState({});
+
+  useEffect(() => {
+    localStorage.setItem('pacientes', JSON.stringify(pacientes)); // agregando localStorage para los pacientes
+  }, [pacientes])
 
   const eliminarPaciente = (id) => {
     const pacientesActualizados = pacientes.filter( paciente => paciente.id !== id); // eliminar paciente del state
